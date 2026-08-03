5a9960c03aebc4b615b965c199f3b3b2493f08ec
Sanskar Arnale <sanskararnale07@gmail.com>
2026-08-03 20:30:54 +0530

    Add binary search implementation in a new file

--- Bit Manipulation/704. Binary Search.cpp
+++ Bit Manipulation/704. Binary Search.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
+    int search(vector<int>& nums, int target) {
         int low = 0;
         int high = nums.size() - 1;
-        while(low <= high){
-            int mid = (low + high)/2;
+        while(low <= high)
+        {
+            int mid = (low+high)/2;
             if(nums[mid] == target)
             {
                 return mid;
@@ -20,6 +21,6 @@ public:
                 high = mid - 1;
             }
         }
-        return low;
+        return -1;
     }
 };
